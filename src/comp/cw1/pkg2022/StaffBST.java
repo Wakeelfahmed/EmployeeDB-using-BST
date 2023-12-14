bddbb9928d8865ffeec13f85f73c104754b2fd9c
Wakeel Furqan Ahmed <wakeelfahmed@gmail.com>
2023-12-15 01:36:58 +0500

    Employee DB using BST fully done

--- src/comp/cw1/pkg2022/StaffBST.java
+++ src/comp/cw1/pkg2022/StaffBST.java
@@ -1,4 +1,5 @@
 package comp.cw1.pkg2022;
+import javax.swing.text.StyledEditorKit;
 import java.io.FileWriter;
 import java.io.IOException;
 public class StaffBST implements IStaffDB {
@@ -38,26 +39,57 @@ public class StaffBST implements IStaffDB {
     }
 
     @Override
-    public boolean containsName(String name) { return get(name) != null; }
+    public boolean containsName(String name) {
+        assert name != null && !name.isEmpty() : "Invalid name given to check";
 
-    private Employee retrieve(Node tree, String name) {
+        Employee result = retrieve(root, name, false);
+        //Employee result = get(name);
+        if (result != null) {
+            logEmployeeInfo("\nContains", name, "\tEmployee found");
+            return true;
+        } else {
+            logEmployeeInfo("\nContains", name, "\tEmployee not found");
+            return false;
+        }
+    }
+   // public boolean containsName(String name) { return get(name) != null; }
+
+    private Employee retrieve(Node tree, String name, Boolean Log) {
         if (tree == null) {
+            if(Log)
+                logEmployeeInfo("");
             return null;
         }
 
         int compareResult = name.compareTo(tree.data.getName());
 
         if (compareResult < 0) {
-            return retrieve(tree.left, name);
+            if(Log)
+                logEmployeeInfo("  Going left", tree.data);
+            return retrieve(tree.left, name, Log);
         } else if (compareResult > 0) {
-            return retrieve(tree.right, name);
+            if(Log)
+                logEmployeeInfo("  Going right", tree.data);
+            return retrieve(tree.right, name, Log);
         } else {
+            if(Log)
+                logEmployeeInfo("  Found", tree.data);
             return tree.data;
         }
     }
 
     @Override
-    public Employee get(String name) { return retrieve(root, name); }
+    public Employee get(String name) {
+        assert name != null && !name.isEmpty() : "Invalid name for get";
+
+        logEmployeeInfo("\nGet " + name +"\n");
+        Employee result = retrieve(root, name, true);
+        if (result == null) {
+            logEmployeeInfo("  " + name + " not found");
+        }
+        return result;
+    }
+    //public Employee get(String name) { return retrieve(root, name); }
 
     @Override
     public int size() { return numEntries; }
@@ -66,34 +98,46 @@ public class StaffBST implements IStaffDB {
     public boolean isEmpty() { return size() == 0; }
 
     private Node insert(Node tree, Employee employee) {
-        if (tree == null) {
+        assert employee != null : "Null employee cannot be added";
+
+        if (tree == null && tree == root) {
+            logEmployeeInfo("  Insertion in Empty tree", employee);
+            return new Node(employee);
+        }
+        else if (tree == null) {
+            logEmployeeInfo("  Insertion", employee);
             return new Node(employee);
         }
 
         int compareResult = employee.getName().compareTo(tree.data.getName());
 
         if (compareResult < 0) {
+            logEmployeeInfo("  Going left", tree.data);
+            if(tree.left == null)
+                logEmployeeInfo("  Found empty node to insert\n");
             tree.left = insert(tree.left, employee);
         } else if (compareResult > 0) {
+            logEmployeeInfo("  Going right", tree.data);
+            if(tree.right == null) {
+                logEmployeeInfo("  Found  empty node to insert\n");
+            }
             tree.right = insert(tree.right, employee);
         } else {
             // Key already exists, update the value or handle accordingly
             tree.data = employee;
+            logEmployeeInfo("  Updation", employee);
         }
-
         return tree;
     }
 
     @Override
     public Employee put(Employee member) {
-        Employee employee_test = get(member.getName());
+        logEmployeeInfo("\nPut", member);
+        Employee employee_test = retrieve(root, member.getName(), false);
         root = insert(root, member);
-        if(employee_test != null){
-            logEmployeeInfo("Updation", member);
-            return employee_test;
-        }
+        if(employee_test != null)
+            return member;
         else{
-            logEmployeeInfo("Insertion", member);
             numEntries++;
             return null;
         }
@@ -101,16 +145,25 @@ public class StaffBST implements IStaffDB {
 
     @Override
     public  Employee remove(String name){
+        assert name != null && !name.isEmpty() : "Invalid name for removal";
+
+        logEmployeeInfo("Remove ", "", name);
 
         Node parent = null, del, p = null, q = null;
         Employee result;
         del = root;
         while (del != null && !del.data.getName().equals(name)) {
             parent = del;
-            if (name.compareTo(del.data.getName()) < 0)
+            if (name.compareTo(del.data.getName()) < 0){
                 del = del.left;
-            else
+                if(del != null)
+                    logEmployeeInfo("  Remove (Going left)", del.data);
+            }
+            else{
                 del = del.right;
+                if(del != null)
+                    logEmployeeInfo("  Remove (Going right)", del.data);
+            }
         }// del == null || del.data.getName().equals(name))
         if(del != null) { // del.data.getName().equals(name)
             // find the pointer p to the node to replace del
@@ -128,15 +181,19 @@ public class StaffBST implements IStaffDB {
             else parent.right = p;
             numEntries--;
             result = del.data;
-            logEmployeeInfo("Deletion", result);
+            logEmployeeInfo("  Remove", result);
         }
-        else result = null;
+        else {
+            result = null;
+            logEmployeeInfo("  Remove", name, "\tnot found for removal");
+        }
         return result;
     } // delete
 
     private void traverse(Node tree) {
         if (tree != null) {
             traverse(tree.left);
+            logEmployeeInfo("  " + tree.data.getName() + "\tAffiliation" + tree.data.getAffiliation() + "\n");
             System.out.println(tree.data.toString()); // or log to file
             traverse(tree.right);
         }
@@ -144,13 +201,39 @@ public class StaffBST implements IStaffDB {
 
     @Override
     public void displayDB() {
+        logEmployeeInfo("\nDisplay");
+        logEmployeeInfo("\n");
         traverse(root);
     }
-        public void logEmployeeInfo(String operation, Employee employee) {
+
+    public void logEmployeeInfo(String message){
         try {
-            logFile.write("Operation: " + operation + "\n");
-            logFile.write("Employee Name: " + employee.getName() + "\n");
+            logFile.write(message);
             logFile.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void logEmployeeInfo(String operation, Employee employee) {
+        try {
+            if (employee != null) {
+                logFile.write(operation + ":\t");
+                logFile.write("Employee Name: " + employee.getName() + "\tAffiliation: " + employee.getAffiliation() + "\n");
+                logFile.flush();
+            } else {
+                logFile.write(operation + ": Employee not found - ");
+                logFile.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void logEmployeeInfo(String operation, String name, String message) {
+        try {
+                logFile.write(operation + ":\t" + "Employee Name: " + name + message + "\n");
+                logFile.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/comp/cw1/pkg2022/StaffBSTTest.java
+++ src/comp/cw1/pkg2022/StaffBSTTest.java
@@ -3,18 +3,124 @@ package comp.cw1.pkg2022;
 
 public class StaffBSTTest {
     public void Run() {
-        testBasicInsertionAndRetrieval();   // Test Case 1: Basic Insertion and Retrieval
-        testDeletionAndReinsertion();       // Test Case 2: Deletion and Re-insertion
-        testsize_changing();                // Test Case 3: Size changing
-        testDisplayEmptyDatabase();         // Test Case 4: Displaying an Empty Database
-        testSizeAndEmptyCheck();            // Test Case 5: Testing Size and Empty Check
-        testRetrieveNonExistentEmployee();  // Test Case 6: Retrieving Non-Existent Employee
-        testRemoveNonExistentEmployee();    // Test Case 7: Removing Non-Existent Employee
+        LeafNodeRemoval();                  // Test Case 1: Leaf Node Removal
+        NodewithOneDescendantDeletion();    // Test Case 2: Removing Node with One Descendant
+        NodewithTwoDescendantDeletion();    // Test Case 3: Removing Node with Two Descendant
+        testBasicInsertionAndRetrieval();   // Test Case 4: Basic Insertion and Retrieval
+        testDeletionAndReinsertion();       // Test Case 5: Deletion and Re-insertion
+        testsize_changing();                // Test Case 6: Size changing
+        testDisplayEmptyDatabase();         // Test Case 7: Displaying an Empty Database
+        testSizeAndEmptyCheck();            // Test Case 8: Testing Size and Empty Check
+        testRetrieveNonExistentEmployee();  // Test Case 9: Retrieving Non-Existent Employee
+        testRemoveNonExistentEmployee();    // Test Case 10: Removing Non-Existent Employee
     }
-    // Test Case 1: Basic Insertion and Retrieval
-    public static void testBasicInsertionAndRetrieval() {
-        StaffBST StaffBST = new StaffBST();
+
+    // Test Case 1: Leaf Node Removal
+    public static void LeafNodeRemoval() {
         System.out.println("\nTest 1");
+        // Assume the tree is initially empty
+        StaffBST staffBST = new StaffBST();
+        Employee employee1 = new Employee("John", "Company A");
+        Employee employee2 = new Employee("Alice", "Company B");
+        Employee employee3 = new Employee("Bob", "Company C");
+
+        staffBST.put(employee1);
+        staffBST.put(employee2);
+        staffBST.put(employee3);
+
+        // Display the tree before deletion
+        System.out.println("Before deletion:");
+        staffBST.displayDB();
+
+        // Delete a leaf node (e.g., Bob)
+        Employee removedEmployee = staffBST.remove("Bob");
+
+        System.out.println("\nAfter deletion:");
+        staffBST.displayDB();
+
+        // Ensure the removed employee is Bob and the tree is updated correctly
+        assert removedEmployee != null && removedEmployee.getName().equals("Bob");
+        assert !staffBST.containsName("Bob");
+
+        if (!staffBST.containsName("Bob"))
+            System.out.println("Test Case 1 (Deleting a Leaf Node): Pass");
+        else
+            System.out.println("Test Case 1 (Deleting a Leaf Node): Fail");
+    }
+
+    // Test Case 2: Node with One Descendant Deletion
+    public static void NodewithOneDescendantDeletion(){
+        System.out.println("\nTest 2");
+        // Assume the tree is initially empty
+        StaffBST staffBST = new StaffBST();
+        Employee employee1 = new Employee("John", "Company A");
+        Employee employee2 = new Employee("Alice", "Company B");
+
+        staffBST.put(employee1);
+        staffBST.put(employee2);
+
+        // Display the tree before deletion
+        System.out.println("Before deletion:");
+        staffBST.displayDB();
+
+        // Delete a node with one descendant (e.g., John)
+        Employee removedEmployee = staffBST.remove("John");
+
+        // Display the tree after deletion
+        System.out.println("After deletion:");
+        staffBST.displayDB();
+
+        // Ensure the removed employee is John and the tree is updated correctly
+        assert removedEmployee != null && removedEmployee.getName().equals("John");
+        assert !staffBST.containsName("John");
+        if (!staffBST.containsName("John"))
+            System.out.println("Test Case 2 (Deleting a Node with One Descendant): Pass");
+        else
+            System.out.println("Test Case 2 (Deleting a Node with One Descendant): Fail");
+    }
+
+    // Test Case 3: Leaf Node Removal
+    public static void NodewithTwoDescendantDeletion(){
+        System.out.println("\nTest 3");
+
+        // Assume the tree is initially empty
+        StaffBST staffBST = new StaffBST();
+        Employee employee1 = new Employee("John", "Company A");
+        Employee employee2 = new Employee("Alice", "Company B");
+        Employee employee3 = new Employee("Bob", "Company C");
+        Employee employee4 = new Employee("Zara", "Company D");
+
+        staffBST.put(employee1);
+        staffBST.put(employee2);
+        staffBST.put(employee3);
+        staffBST.put(employee4);
+
+        // Display the tree before deletion
+        System.out.println("Before deletion:");
+        staffBST.displayDB();
+
+        // Delete a node with two descendants (e.g., John)   //
+        Employee removedEmployee = staffBST.remove("John");
+
+        // Display the tree after deletion
+        System.out.println("After deletion:");
+        staffBST.displayDB();
+
+        // Ensure the removed employee is John and the tree is updated correctly
+        assert removedEmployee != null && removedEmployee.getName().equals("John");
+        assert !staffBST.containsName("John");
+
+        if (!staffBST.containsName("John")) {
+            System.out.println("Test Case 3 (Deleting a Node with Two Descendants): Pass");
+        } else {
+            System.out.println("Test Case 3 (Deleting a Node with Two Descendants): Fail");
+        }
+    }
+
+    // Test Case 4: Basic Insertion and Retrieval
+    public static void testBasicInsertionAndRetrieval() {
+        System.out.println("\nTest 4");
+        StaffBST StaffBST = new StaffBST();
         Employee employee1 = new Employee("John, Doe", "HR");
         Employee employee2 = new Employee("Jane, Smith", "Finance");
 
@@ -33,14 +139,14 @@ public class StaffBSTTest {
         System.out.println(retrieved2.toString());
 
         if (retrieved1 != null && retrieved1.equals(employee1) && retrieved2 != null && retrieved2.equals(employee2))
-            System.out.println("Test Case 1 (Basic Insertion and Retrieval): Passed");
+            System.out.println("Test Case 4 (Basic Insertion and Retrieval): Passed");
         else
-            System.out.println("Test Case 1 (Basic Insertion and Retrieval): FAILED");
+            System.out.println("Test Case 4 (Basic Insertion and Retrieval): FAILED");
     }
 
-    // Test Case 2: Deletion and Re-insertion
+    // Test Case 5: Deletion and Re-insertion
     public static void testDeletionAndReinsertion() {
-        System.out.println("\nTest 2");
+        System.out.println("\nTest 5");
         StaffBST StaffBST = new StaffBST();
         Employee employee = new Employee("Alice, Johnson", "Marketing");
 
@@ -75,12 +181,12 @@ public class StaffBSTTest {
                 System.out.println("Test Case 2 (Deletion and Re-insertion): FAILED (Re-insertion)");
 
         } else
-            System.out.println("Test Case 2 (Deletion and Re-insertion): FAILED");
+            System.out.println("Test Case 5 (Deletion and Re-insertion): FAILED");
     }
 
-    // Test Case 3: Size Changing
+    // Test Case 6: Size Changing
     public static void testsize_changing() {
-        System.out.println("\nTest 3");
+        System.out.println("\nTest 6");
         StaffBST StaffBST = new StaffBST();
 
         Employee employee1 = new Employee("John, Doe", "HR");
@@ -99,31 +205,32 @@ public class StaffBSTTest {
         System.out.println("Inserting \"Bob, Brown, Sales\"\t\t");
         StaffBST.put(employee4);
 
-        System.out.println("Tree size after 4 records inserted :" + StaffBST.size());
+        System.out.println("Tree size after 6 records inserted :" + StaffBST.size());
 
         if (StaffBST.size() == 4 && StaffBST.get("John, Doe") != null)
-            System.out.println("Test Case 3 (Size changing): Passed");
+            System.out.println("Test Case 6 (Size changing): Passed");
         else
-            System.out.println("Test Case 3 (Size changing): FAILED");
+            System.out.println("Test Case 6 (Size changing): FAILED");
     }
 
-    // Test Case 5: Displaying an Empty Database
+    // Test Case 7: Displaying an Empty Database
     public static void testDisplayEmptyDatabase() {
-        System.out.println("\nTest 4");
+        System.out.println("\nTest 7");
         StaffBST StaffBST = new StaffBST();
         StaffBST.displayDB();
-        System.out.println("Test Case 4 (Displaying an Empty Database): Passed (Visual Check)");
+        System.out.println("Test Case 7 (Displaying an Empty Database): Passed (Visual Check)");
     }
-    // Test Case 6: Testing Size and Empty Check
+
+    // Test Case 8: Testing Size and Empty Check
     public static void testSizeAndEmptyCheck() {
-        System.out.println("\nTest 5");
+        System.out.println("\nTest 8");
         StaffBST StaffBST = new StaffBST();
 
         // Initially, the Tree table is empty
         if (StaffBST.isEmpty() && StaffBST.isEmpty())
-            System.out.println("Test Case 5 (Size and Empty Check - Empty Table): Passed");
+            System.out.println("Test Case 8 (Size and Empty Check - Empty Table): Passed");
         else
-            System.out.println("Test Case 5 (Size and Empty Check - Empty Table): FAILED");
+            System.out.println("Test Case 8 (Size and Empty Check - Empty Table): FAILED");
 
         // Insert a single employee and check size and emptiness
         Employee employee = new Employee("Alice, Johnson", "Marketing");
@@ -131,13 +238,14 @@ public class StaffBSTTest {
         StaffBST.put(employee);
 
         if (StaffBST.size() == 1 && !StaffBST.isEmpty())
-            System.out.println("Test Case 5 (Size and Empty Check - Non-empty Table): Passed");
+            System.out.println("Test Case 8 (Size and Empty Check - Non-empty Table): Passed");
         else
-            System.out.println("Test Case 5 (Size and Empty Check - Non-empty Table): FAILED");
+            System.out.println("Test Case 8 (Size and Empty Check - Non-empty Table): FAILED");
     }
-    // Test Case 7: Retrieving Non-Existent Employee
+
+    // Test Case 9: Retrieving Non-Existent Employee
     public static void testRetrieveNonExistentEmployee() {
-        System.out.println("\nTest 6");
+        System.out.println("\nTest 9");
         StaffBST StaffBST = new StaffBST();
 
         // Try to retrieve an employee that doesn't exist in the table
@@ -145,24 +253,25 @@ public class StaffBSTTest {
         System.out.print("Retrieving \"Nonexistent, Employee\"\t\tGot ->\t");
         if (retrieved == null) {
             System.out.println("Not Found");
-            System.out.println("Test Case 6 (Retrieving Non-Existent Employee): Passed");
+            System.out.println("Test Case 9 (Retrieving Non-Existent Employee): Passed");
         } else {
             System.out.println("Found");
-            System.out.println("Test Case 6 (Retrieving Non-Existent Employee): FAILED");
+            System.out.println("Test Case 9 (Retrieving Non-Existent Employee): FAILED");
         }
     }
-    // Test Case 8: Removing Non-Existent Employee
-    public static void testRemoveNonExistentEmployee() {
-        System.out.println("\nTest 7");
-        StaffBST StaffBST = new StaffBST();
 
+    // Test Case 9: Removing Non-Existent Employee
+    public static void testRemoveNonExistentEmployee() {
+        System.out.println("\nTest 10");
+        StaffBST StaffBST = new StaffBST();
+        System.out.println("Deleting \"Nonexistent Employee\"");
         // Try to remove an employee that doesn't exist in the table
-        Employee removed = StaffBST.remove("Nonexistent, Employee");
+        Employee removed = StaffBST.remove("Nonexistent Employee");
 
         if (removed == null)
-            System.out.println("Test Case 7 (Removing Non-Existent Employee): Passed");
+            System.out.println("Test Case 10 (Removing Non-Existent Employee): Passed");
         else
-            System.out.println("Test Case 7 (Removing Non-Existent Employee): FAILED");
+            System.out.println("Test Case 10 (Removing Non-Existent Employee): FAILED");
     }
 }
 
